2f1e3a61c44d9661152994eb5320a6ac874b6cbc
Aliaksandr Ihnashevich <al.ignashevich@gmail.com>
2021-12-14 02:50:36 +0200

    commit

--- pages/home.server.js
+++ pages/home.server.js
@@ -6,9 +6,10 @@ const Home = () => {
             <button className="text-4xl sm:text-6xl">Aliaksandr Ihnashevich</button>
             <a className="text-xl sm:text-4xl hover:text-gray-700" href="mailto:al.ignashevich@gmail.com">al.ignashevich@gmail.com</a>
             <div className="sm:text-2xl flex space-x-6">
-                <a className="hover:text-gray-700" href="tg://resolve?domain=al_ignashevich">Telegram</a>
-                <a className="hover:text-gray-700" href="https://github.com/ignashevich">Github</a>
-                <a className="hover:text-gray-700" href="https://www.linkedin.com/in/aliaksandr-ihnashevich-775540129/">Linkedin</a>
+                <a rel="noreferrer" target="_blank" className="hover:text-gray-700" href="https://docs.google.com/document/d/1S7TfClSn_sQU1VjOiVaRVpX4PXeWa00c6g0zGe39yRc/edit?usp=sharing">Resume</a>
+                <a rel="noreferrer" target="_blank" className="hover:text-gray-700" href="tg://resolve?domain=al_ignashevich">Telegram</a>
+                <a rel="noreferrer" target="_blank" className="hover:text-gray-700" href="https://github.com/ignashevich">Github</a>
+                <a rel="noreferrer" target="_blank" className="hover:text-gray-700" href="https://www.linkedin.com/in/aliaksandr-ihnashevich-775540129/">Linkedin</a>
             </div>
         </main>
     );
