a8439b1e987515df31c0a9d9931594a5563b67af
Aliaksandr Ihnashevich <al.ignashevich@gmail.com>
2021-12-14 02:54:05 +0200

    commit

--- pages/home.server.js
+++ pages/home.server.js
@@ -5,7 +5,7 @@ const Home = () => {
         <main className="grid h-full gap-3 place-content-center place-items-center px-8">
             <button className="text-4xl sm:text-6xl">Aliaksandr Ihnashevich</button>
             <a className="text-xl sm:text-4xl hover:text-gray-700" href="mailto:al.ignashevich@gmail.com">al.ignashevich@gmail.com</a>
-            <div className="sm:text-2xl flex space-x-6">
+            <div className="sm:text-2xl flex space-x-2 sm:flex space-x-6">
                 <a rel="noreferrer" target="_blank" className="hover:text-gray-700" href="https://docs.google.com/document/d/1S7TfClSn_sQU1VjOiVaRVpX4PXeWa00c6g0zGe39yRc/edit?usp=sharing">Resume</a>
                 <a rel="noreferrer" target="_blank" className="hover:text-gray-700" href="tg://resolve?domain=al_ignashevich">Telegram</a>
                 <a rel="noreferrer" target="_blank" className="hover:text-gray-700" href="https://github.com/ignashevich">Github</a>
