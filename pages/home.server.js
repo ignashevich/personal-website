0c52e154fba51fd2b946b4397f8e54a9b17bb658
Aliaksandr Ihnashevich <al.ignashevich@gmail.com>
2021-12-14 03:26:22 +0200

    commit

--- pages/home.server.js
+++ pages/home.server.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <main className="grid h-full gap-3 place-content-center place-items-center px-8">
-            <button className="text-4xl sm:text-6xl">Aliaksandr Ihnashevich</button>
+            <h1 className="text-4xl sm:text-6xl">Aliaksandr Ihnashevich</h1>
             <a className="text-xl sm:text-4xl hover:text-gray-700" href="mailto:al.ignashevich@gmail.com">al.ignashevich@gmail.com</a>
             <div className="sm:text-2xl flex space-x-2 sm:space-x-6">
                 <a rel="noreferrer" target="_blank" className="hover:text-gray-700" href="https://docs.google.com/document/d/1S7TfClSn_sQU1VjOiVaRVpX4PXeWa00c6g0zGe39yRc/edit?usp=sharing">Resume</a>
