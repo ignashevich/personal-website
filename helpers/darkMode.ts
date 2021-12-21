49bc10f04619edfe725b9d6c1a8045b186364355
Aliaksandr Ihnashevich <al.ignashevich@gmail.com>
2021-12-21 23:42:10 +0200

    commit

--- helpers/darkMode.ts
+++ helpers/darkMode.ts
@@ -1,23 +1,23 @@
 export class DarkMode {
     static off() {
         // @ts-ignore
-        if (window !== 'undefined') {
+        if (typeof window !== 'undefined') {
             document.documentElement.classList.remove('dark');
             localStorage.theme = 'light';
         }
     }
     static on() {
         // @ts-ignore
-        if (window !== 'undefined') {
+        if (typeof window !== 'undefined') {
             document.documentElement.classList.add('dark');
             localStorage.theme = 'dark';
         }
     }
     static isEnabled() {
         // @ts-ignore
-        if (window !== 'undefined') {
+        if (typeof window !== 'undefined') {
             return document.documentElement.classList.contains('dark');
-        }
+        } else return false;
     }
 
 }
--- helpers/useValue.ts
+++ helpers/useValue.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export const useValue = <T = undefined>(defaultValue: T) => {
+export const useValue = <T = undefined>(defaultValue: T | (() => T)) => {
     const [value, set] = useState<T>(defaultValue);
 
     return ({get: () => value, set});
