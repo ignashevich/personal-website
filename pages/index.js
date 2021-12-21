265d9c9aed4dd7295d8d17f4a458b66f0698ae67
Aliaksandr Ihnashevich <al.ignashevich@gmail.com>
2021-12-21 21:23:24 +0200

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Head from "../components/utils/Head.js";
 import Home from "./home";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "../components/Header.tsx";
+import Footer from "../components/Footer.tsx";
 
 
 export default function Index() {
