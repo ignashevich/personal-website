696f058d3bb46378a48ffa741341ea4a849a65f3
Aliaksandr Ihnashevich <al.ignashevich@gmail.com>
2021-12-21 21:34:13 +0200

    commit

--- helpers/darkMode.ts
+++ helpers/darkMode.ts
@@ -1,17 +1,20 @@
 export class DarkMode {
     static off() {
+        // @ts-ignore
         if (window !== 'undefined') {
             document.documentElement.classList.remove('dark');
             localStorage.theme = 'light';
         }
     }
     static on() {
+        // @ts-ignore
         if (window !== 'undefined') {
             document.documentElement.classList.add('dark');
             localStorage.theme = 'dark';
         }
     }
     static isEnabled() {
+        // @ts-ignore
         if (window !== 'undefined') {
             return localStorage.theme === 'dark';
         }
