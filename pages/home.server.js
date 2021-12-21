f02467ff49f795c0cd0c62cd61357a94dd56e0bf
Aliaksandr Ihnashevich <al.ignashevich@gmail.com>
2021-12-21 03:13:26 +0200

    commit

--- pages/home.server.js
+++ pages/home.server.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Image from 'next/image'
 
 const Home = () => {
     return (
         <main className="grid h-full gap-3 place-content-center place-items-center px-8">
+            <div className="mb-4 md:mb-8 w-6/12 md:w-fit">
+                <Image width={240} height={220 } className="rounded-lg" src="/me.png" alt="Photo of me"/>
+            </div>
             <h1 className="text-center text-4xl sm:text-6xl">Aliaksandr Ihnashevich</h1>
             <a className="text-xl sm:text-4xl hover:text-gray-700" href="mailto:al.ignashevich@gmail.com">al.ignashevich@gmail.com</a>
             <div className="sm:text-2xl flex space-x-2 sm:space-x-6">
