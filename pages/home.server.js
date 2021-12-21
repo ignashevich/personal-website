b1999164c212d1c2009c237f1ef3199daa828f4c
Aliaksandr Ihnashevich <al.ignashevich@gmail.com>
2021-12-21 03:44:38 +0200

    commit

--- pages/home.server.js
+++ pages/home.server.js
@@ -9,12 +9,26 @@ const Home = () => {
             </div>
             <h1 className="text-center text-4xl sm:text-6xl">Aliaksandr Ihnashevich</h1>
             <a className="text-xl sm:text-4xl hover:text-gray-700" href="mailto:al.ignashevich@gmail.com">al.ignashevich@gmail.com</a>
-            <div className="sm:text-2xl flex space-x-2 sm:space-x-6">
-                <a rel="noreferrer" target="_blank" className="hover:text-gray-700" href="https://docs.google.com/document/d/1S7TfClSn_sQU1VjOiVaRVpX4PXeWa00c6g0zGe39yRc/edit?usp=sharing">Resume</a>
-                <a rel="noreferrer" target="_blank" className="hover:text-gray-700" href="tg://resolve?domain=al_ignashevich">Telegram</a>
-                <a rel="noreferrer" target="_blank" className="hover:text-gray-700" href="https://github.com/ignashevich">Github</a>
-                <a rel="noreferrer" target="_blank" className="hover:text-gray-700" href="https://www.linkedin.com/in/aliaksandr-ihnashevich-775540129/">Linkedin</a>
+            <div className="sm:text-2xl flex space-x-2 sm:space-x-6 mb-6 sm:mb-8">
+                <a rel="noreferrer" target="_blank" className="text-blue-500 hover:text-gray-400" href="https://docs.google.com/document/d/1S7TfClSn_sQU1VjOiVaRVpX4PXeWa00c6g0zGe39yRc/edit?usp=sharing">Resume</a>
+                <a rel="noreferrer" target="_blank" className="text-blue-500 hover:text-gray-400" href="tg://resolve?domain=al_ignashevich">Telegram</a>
+                <a rel="noreferrer" target="_blank" className="text-blue-500 hover:text-gray-400" href="https://github.com/ignashevich">Github</a>
+                <a rel="noreferrer" target="_blank" className="text-blue-500 hover:text-gray-400" href="https://www.linkedin.com/in/aliaksandr-ihnashevich-775540129/">Linkedin</a>
             </div>
+            <div className="justify-self-start">
+                <h2 className="text-xl sm:text-4xl mb-4 sm:mb-6">
+                    My personal projects:
+                </h2>
+                <ul className="sm:text-2xl space-y-2">
+                    <li>
+                        - Tulips online shop (<a rel="noreferrer" target="_blank" className="text-blue-500 hover:text-blue-400" href="https://tulpan-mart.by">tulpan-mart.by</a>)
+                    </li>
+                    <li>
+                        - Landing page for the game (<a rel="noreferrer" target="_blank" className="text-blue-500 hover:text-blue-400" href="https://catellite.space">catellite.com</a>)
+                    </li>
+                </ul>
+            </div>
+
         </main>
     );
 };
