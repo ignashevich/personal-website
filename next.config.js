5ef36f36d685a455b31512013ea764cd943e0eaa
Aliaksandr Ihnashevich <al.ignashevich@gmail.com>
2021-12-25 00:58:02 +0200

    add framer feature

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     reactStrictMode: true,
+    experimental: {
+        urlImports: [
+            "https://framer.com/m/",
+            "https://framerusercontent.com/",
+            "https://ga.jspm.io/",
+            "https://jspm.dev/",
+        ],
+    },
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/,
