f9ae78bab1f08d705f7633cbee3367812c351ebc
Aliaksandr Ihnashevich <al.ignashevich@gmail.com>
2022-01-09 02:27:12 +0200

    fix import error

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,12 @@
 import NextLink from 'next/link';
-import React, { useCallback, useLayoutEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { DarkMode } from '../helpers/darkMode';
 import { useValue } from '../helpers/useValue';
-import Logo from './logo.svg';
 import { useRouter } from 'next/router';
 import cx from 'classnames';
-import Burger from '@animated-burgers/burger-slide';
 import '@animated-burgers/burger-slide/dist/styles.css';
 import DarkModeToggle from '../components/DarkModeToggle';
-import { Sling as Hamburger } from 'hamburger-react'
+import { Sling as Hamburger } from 'hamburger-react';
 
 
 const Header = () => {
@@ -28,11 +26,11 @@ const Header = () => {
 
     return (
         <header className="flex items-center justify-between px-8 py-8 sm:py-12">
-            {/*<Logo className="dark:fill-black dark:stroke-black dark:fill-white dark:stroke-white w-10 md:w-14"/>*/}
-            <DarkModeToggle value={isDarkModeEnabled} className="lg:hidden block"/>
+            {/*<Logo className="dark:fill-black dark:stroke-black dark:fill-white dark:stroke-white w-10 md:w-14"/>*/ }
+            <DarkModeToggle value={ isDarkModeEnabled } className="lg:hidden block"/>
 
             <nav className={ cx('lg:block', !isBurgerOpened && 'hidden', isBurgerOpened && 'absolute top-0 left-0 bg-white dark:bg-black w-screen h-screen z-10') }>
-                <ul className={cx("lg:flex container", !isBurgerOpened && "space-x-8", isBurgerOpened && "space-y-4 text-right mx-auto pr-8")}>
+                <ul className={ cx('lg:flex container', !isBurgerOpened && 'space-x-8', isBurgerOpened && 'space-y-4 text-right mx-auto pr-8') }>
                     <li className="mt-28 lg:mt-0">
                         <Link href="/">Home</Link>
                     </li>
@@ -45,9 +43,9 @@ const Header = () => {
                 </ul>
             </nav>
 
-            <DarkModeToggle value={isDarkModeEnabled} className="hidden lg:block"/>
+            <DarkModeToggle value={ isDarkModeEnabled } className="hidden lg:block"/>
             <button className="block lg:hidden z-20" onClick={ () => setIsBurgerOpened(v => !v) }>
-                <Hamburger size={42} toggled={ isBurgerOpened }/>
+                <Hamburger size={ 42 } toggled={ isBurgerOpened }/>
             </button>
         </header>
     );
